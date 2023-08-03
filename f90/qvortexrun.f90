d828554ee2d177ce1f06c5bcb97dcca276f85bf8
Joon <jun9303@gmail.com>
2023-08-03 09:20:58 -0700

    modified:   f90/dustyqvortrun.f90 	modified:   f90/qvortexrun.f90

--- f90/qvortexrun.f90
+++ f90/qvortexrun.f90
@@ -165,7 +165,8 @@ CONTAINS
       OPEN(FU, FILE=OUTPUTPATH)
 
       WRITE(FU,'(A110)')'variables= "x","y","z","ux","uy","uz","wx","wy","wz"'
-      WRITE(FU,*)'ZONE T=', NTOA(TINFO%T,'(F8.3)'), ', I=',INUM,', J=',JNUM,', K=',KNUM,', ZONETYPE=Ordered'
+      WRITE(FU,*)'ZONE T="', TRIM(ADJUTL(NTOA(TINFO%T,'(F8.3)'))), '", I=',INUM,', J=',JNUM,', K=',KNUM, &
+                 ', ZONETYPE=Ordered'
       WRITE(FU,*)'DATAPACKING=POINT'
       DO K=1,FINFO%NZ+1,ZSKIP
         DO J=1,2*FINFO%NPH+1,PSKIP
@@ -207,7 +208,7 @@ CONTAINS
         ENDDO
       ENDDO
       CLOSE(FU)
-      
+
       CALL DEALLOC( O )
       
 103   FORMAT(8E23.15)
